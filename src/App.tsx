5f03ace421e9818d0cdf125b72d726ca47c96a0a
Florian Selent <florian-github@selent.me>
2022-08-11 16:26:23 +0000

    Starter version.

--- src/App.tsx
+++ src/App.tsx
@@ -3,71 +3,31 @@ import './index.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronRight, faChevronLeft, faCircle, faCheckCircle, faPlus } from '@fortawesome/free-solid-svg-icons';
 
-type ShoppingItem = {
-	itemName: string,
-	quantity: number,
-	isSelected: boolean
+interface ShoppingItem {
+	itemName: string
 }
 
 const App = () => {
 	// HINT: each "item" in our list names a name,
 	// a boolean to tell if its been completed, and a quantity
-	const [items, setItems] = useState<ShoppingItem[]>([
-		{ itemName: 'item 1', quantity: 1, isSelected: false },
-		{ itemName: 'item 2', quantity: 3, isSelected: true },
-		{ itemName: 'item 3', quantity: 2, isSelected: false },
-	]);
-
+	const [items, setItems] = useState<string[]>(['item 1', 'item 2', 'item 3']);
 	const [inputValue, setInputValue] = useState('');
-	const [totalItemCount, setTotalItemCount] = useState(6);
 
 	const handleAddButtonClick = () => {
-		const newItem: ShoppingItem = {
-			itemName: inputValue,
-			quantity: 1,
-			isSelected: false,
-		};
-
-		const newItems = [...items, newItem];
+		const newItems = [...items, inputValue];
 
 		setItems(newItems);
 		setInputValue('');
-		calculateTotal();
+		console.log(newItems);
 	};
 
-	const handleQuantityIncrease = (index: number) => {
-		const newItems = [...items];
+	const handleQuantityDecrease = () => {
+		console.log("decrease");
+	}
 
-		newItems[index].quantity++;
-
-		setItems(newItems);
-		calculateTotal();
-	};
-
-	const handleQuantityDecrease = (index: number) => {
-		const newItems = [...items];
-
-		newItems[index].quantity--;
-
-		setItems(newItems);
-		calculateTotal();
-	};
-
-	const toggleComplete = (index: number) => {
-		const newItems = [...items];
-
-		newItems[index].isSelected = !newItems[index].isSelected;
-
-		setItems(newItems);
-	};
-
-	const calculateTotal = () => {
-		const totalItemCount = items.reduce((total, item) => {
-			return total + item.quantity;
-		}, 0);
-
-		setTotalItemCount(totalItemCount);
-	};
+	const handleQuantityIncrease = () => {
+		console.log("increase");
+	}
 
 	return (
 		<div className='app-background'>
@@ -77,34 +37,33 @@ const App = () => {
 					<FontAwesomeIcon icon={faPlus} onClick={() => handleAddButtonClick()} />
 				</div>
 				<div className='item-list'>
-					{items.map((item, index) => (
-						<div className='item-container'>
-							<div className='item-name' onClick={() => toggleComplete(index)}>
-								{item.isSelected ? (
-									<>
-										<FontAwesomeIcon icon={faCheckCircle} />
-										<span className='completed'>{item.itemName}</span>
-									</>
-								) : (
-									<>
-										<FontAwesomeIcon icon={faCircle} />
-										<span>{item.itemName}</span>
-									</>
-								)}
-							</div>
-							<div className='quantity'>
-								<button>
-									<FontAwesomeIcon icon={faChevronLeft} onClick={() => handleQuantityDecrease(index)} />
-								</button>
-								<span> {item.quantity} </span>
-								<button>
-									<FontAwesomeIcon icon={faChevronRight} onClick={() => handleQuantityIncrease(index)} />
-								</button>
-							</div>
+					<div className='item-container'>
+						<div className='item-name'>
+							{/* TODO check if the item is selected or not */}
+							{false ? (
+								<>
+									<FontAwesomeIcon icon={faCheckCircle} />
+									<span className='completed'>{items[0]}</span>
+								</>
+							) : (
+								<>
+									<FontAwesomeIcon icon={faCircle} />
+									<span>{items[0]}</span>
+								</>
+							)}
 						</div>
-					))}
+						<div className='quantity'>
+							<button>
+								<FontAwesomeIcon icon={faChevronLeft} onClick={() => handleQuantityDecrease()} />
+							</button>
+							<span> 2 </span>
+							<button>
+								<FontAwesomeIcon icon={faChevronRight} onClick={() => handleQuantityIncrease()} />
+							</button>
+						</div>
+					</div>
 				</div>
-				<div className='total'>Total: {totalItemCount}</div>
+				<div className='total'>Total: 42</div>
 			</div>
 		</div>
 	);
